ac1c3556d0eca8a09b06bf931017c502a841f4c8
Aaron <areina@telecomboutique.com>
2020-02-20 16:20:23 +0100

    first commit

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,45 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Patch,
+  Delete,
+} from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { Task, TaskStatus } from './task.model';
+import { CreateTaskDto } from './dto/create-task.dto';
+
+@Controller('tasks')
+export class TasksController {
+  constructor(private tasksService: TasksService) {}
+
+  @Get()
+  getAllTasks(): Task[] {
+    return this.tasksService.getAllTasks();
+  }
+
+  @Get(':id')
+  getTaskById(@Param('id') id: string): Task {
+    return this.tasksService.getTaskById(id);
+  }
+
+  @Delete(':id')
+  deleteTaskById(@Param('id') id: string): Task[] {
+    return this.tasksService.deleteTaskById(id);
+  }
+
+  @Patch(':id/status')
+  patchTaskById(
+    @Param('id') id: string,
+    @Body('status') status: TaskStatus,
+  ): Task {
+    return this.tasksService.patchTaskById(id, status);
+  }
+
+  @Post()
+  createTask(@Body() createTaskDto: CreateTaskDto): Task {
+    return this.tasksService.createTask(createTaskDto);
+  }
+}
---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
+import * as uuid from 'uuid/v1';
+import { CreateTaskDto } from './dto/create-task.dto';
+
+@Injectable()
+export class TasksService {
+  private tasks: Task[] = [];
+
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
+
+  getTaskById(id: string): Task {
+    return this.tasks.find(e => e.id === id);
+  }
+
+  deleteTaskById(id: string): Task[] {
+    this.tasks = this.tasks.filter(e => e.id !== id);
+    return this.tasks;
+  }
+
+  patchTaskById(id: string, status: TaskStatus) {
+    const task = this.getTaskById(id);
+    task.status = status;
+    return task;
+  }
+
+  createTask(createTaskDto: CreateTaskDto): Task {
+    const { title, description } = createTaskDto;
+
+    const task: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+    this.tasks.push(task);
+    return task;
+  }
+}
